323f3be9a255ecf36ea4d2924973682e8f3340d3
kobano <mlme@disroot.org>
2018-03-02 12:13:06 +0100

    version complète fonctionnelle

--- app.js
+++ app.js
@@ -14,19 +14,28 @@ var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function (socket, pseudo) {
     // Quand un client se connecte, on lui envoie un message
-    socket.emit('message', 'Vous êtes bien connecté !');
+    // socket.emit('message', 'Vous êtes bien connecté !');
     // On signale aux autres clients qu'il y a un nouveau venu
-    socket.broadcast.emit('message', 'Un autre client vient de se connecter ! ');
+    // socket.broadcast.emit('message', 'Un autre client vient de se connecter ! ');
+    // socket.broadcast.emit('chat', socket.pseudo + ' vient de se connecter !');
 
     // Dès qu'on nous donne un pseudo, on le stocke en variable de session
     socket.on('petit_nouveau', function(pseudo) {
         socket.pseudo = pseudo;
+        socket.broadcast.emit('chat', '<i>' + socket.pseudo + ' a rejoint le chat !</i>');
+        socket.emit('chat', '<i>' + socket.pseudo + ' a rejoint le chat !</i>');
     });
 
     // Dès qu'on reçoit un "message" (clic sur le bouton), on le note dans la console
-    socket.on('message', function (message) {
+    socket.on('chat', function (message) {
         // On récupère le pseudo de celui qui a cliqué dans les variables de session
-        console.log(socket.pseudo + ' me parle ! Il me dit : ' + message);
+
+        var mess = '<span style="background-color:#333; color:#BBB;"">' + socket.pseudo + '</span> : ' + message;
+
+        // Envoi du message aux autres
+        socket.broadcast.emit('chat', mess);
+        // Envoi du message sur sa propre page :
+        socket.emit('chat', mess);
     });
 });
 
